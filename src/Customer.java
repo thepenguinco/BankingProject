b0c2383a54a0967910aa3bf55484062608b4ba79
Eric Li <li.eric00@gmail.com>
2017-10-10 22:26:55 -0400

    Start Customer class

--- src/Customer.java
+++ src/Customer.java
@@ -4,10 +4,13 @@
  * @author Eric Li 
  * @version 1.0
  */
+
+//*****************************************************************************|
 public class Customer
 {
     // instance variables
-    private String firstName;
+    
+	private String firstName;
     private String lastName;
     private int sin;
     private int birthYear;
@@ -17,19 +20,30 @@ public class Customer
     private SavingsAccount savingsAccount;
     private CreditCard creditCard;
     
+    // constructors
+    
     /**
-     * Constructs a default Customer object
+     * Constructs a Customer with default characteristics
      */
     public Customer()
     {
-    	firstName = "";
-    	lastName = "";
+    	// TODO
     }
     
     /**
-     * Constructs a Customer object with the specified characteristics
+     * Constructs a customer with specified characteristics
+     * and an initial savings account
+     * 
+     * @param firstName the first name of this customer
+     * @param lastName the last name of this customer
+     * @param sin the social insurance number of this customer
+     * @param birthYear the year this customer was born in
+     * @param birthMonth the month this customer was born in
+     * @param birthDay the day of the month this customer was born on
+     * @param savingsAccount the savings account of this customer
      */
-    public Customer(String firstName, String lastName, int sin, int birthYear, int birthMonth, int birthDay)
+    public Customer(String firstName, String lastName, int sin, 
+    		int birthYear, int birthMonth, int birthDay, SavingsAccount savingsAccount)
     {
         this.firstName = firstName;
         this.lastName = lastName;
@@ -37,17 +51,22 @@ public class Customer
         this.birthYear = birthYear;
         this.birthMonth = birthMonth;
         this.birthDay = birthDay;
+        this.savingsAccount = savingsAccount;
     }
+    
+    // TODO
+    
+    // accessors
 
     /**
-     * An example of a method - replace this comment with your own
+     * Returns the first name of the customer
      * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
+     * @return the first name of the customer 
      */
-    public int sampleMethod(int x, int y)
+    public String getFirstName()
     {
-        // put your code here
-        return x + y;
+        return firstName;
     }
+    
+    // TODO
 }
